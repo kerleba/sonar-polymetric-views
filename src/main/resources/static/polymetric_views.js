095f2f04226cd367ed955ba3265f24422e6d54d0
Klara Erlebachova <klara.erlebachova@gmail.com>
2017-03-10 14:54:53 +0100

    Visualization from static json

--- src/main/resources/static/polymetric_views.js
+++ src/main/resources/static/polymetric_views.js
@@ -1,4 +1,20 @@
 window.registerExtension('sonarPolymetricViews/polymetric_views', function (options) {
-    options.el.textContent = 'This is my page with polymetric views!';
+    var imported = document.createElement('script');
+    document.head.appendChild(imported);
+
+    imported.src = 'https://d3js.org/d3.v4.min.js';
+    imported.onload = function () {
+
+        var header = document.createElement('h2');
+        header.textContent = 'This is my page with polymetric views!';
+        options.el.appendChild(header);
+
+        var p = document.createElement('p');
+        options.el.appendChild(p);
+
+
+    };
+
+
     return function () {};
 });

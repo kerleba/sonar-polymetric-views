cdbfdd84ae095f0ed7ce30ecc63779465025ad19
Klara Erlebachova <klara.erlebachova@gmail.com>
2017-04-11 14:46:12 +0200

    in sonarqube without commented code

--- src/main/resources/static/polymetric_views.js
+++ src/main/resources/static/polymetric_views.js
@@ -5,307 +5,261 @@ window.registerExtension('sonarPolymetricViews/polymetric_views', function (opti
     imported.src = 'https://d3js.org/d3.v4.min.js';
     imported.onload = function () {
 
-        var data = [
-            {
-                "class": "Classname",
-                "metric1": 50,
-                "metric2": 70,
-                "children": [
-                    {
-                        "class": "Classname2",
-                        "metric1": 25,
-                        "metric2": 90,
-                        "children": []
-                    }
-                ]
-            },
-            {
-                "class": "Classname2",
-                "metric1": 50,
-                "metric2": 90,
-                "children": []
-            },
-            {
-                "class": "Classname2",
-                "metric1": 50,
-                "metric2": 30,
-                "children": []
-            },
-            {
-                "class": "Classname2",
-                "metric1": 50,
-                "metric2": 70,
-                "children": [
-                    {
-                        "class": "Classname2",
-                        "metric1": 10,
-                        "metric2": 40
-                    },
-                    {
-                        "class": "Classname2",
-                        "metric1": 40,
-                        "metric2": 90
-                    }
-                ]
-
-            }
-        ];
-
-        var items = [
-            {
-                "class": "Classname",
-                "x": 0,
-                "y": 0,
-                "metric1": 50,
-                "metric2": 70
-            },
-            {
-                "class": "Classname2",
-                "x": 12.5,
-                "y": 80,
-                "metric1": 25,
-                "metric2": 90
-            },
-            {
-                "class": "Classname2",
-                "x": 60,
-                "y": 0,
-                "metric1": 50,
-                "metric2": 90
-            },
-            {
-                "class": "Classname2",
-                "x": 120,
-                "y": 0,
-                "metric1": 50,
-                "metric2": 30
-            },
-            {
-                "class": "Classname2",
-                "x": 180,
-                "y": 0,
-                "metric1": 50,
-                "metric2": 70
-            },
-            {
-                "class": "Classname2",
-                "x": 180,
-                "y": 85,
-                "metric1": 10,
-                "metric2": 40
-            },
-            {
-                "class": "Classname2",
-                "x": 193.5,
-                "y": 85,
-                "metric1": 40,
-                "metric2": 90
-            }
-        ];
-
-        var data2 = [
-            {
-                "class": "Classname",
-                "width": 50,
-                "height": 70,
-                "children_width": 0,
-                "x" : 0,
-                "y" : 0,
-                "children": [
-                    {
-                        "class": "Classname2",
-                        "width": 25,
-                        "height": 90,
-                        "children_width": 0,
-                        "x" : 0,
-                        "y" : 0,
-                        "children": []
-                    }
-                ]
-            },
-            {
-                "class": "Classname2",
-                "width": 50,
-                "height": 90,
-                "children_width": 0,
-                "x" : 0,
-                "y" : 0,
-                "children": []
-            },
-            {
-                "class": "Classname2",
-                "width": 50,
-                "height": 30,
-                "children_width": 0,
-                "x" : 0,
-                "y" : 0,
-                "children": []
-            },
-            {
-                "class": "Classname2",
-                "width": 50,
-                "height": 70,
-                "children_width": 0,
-                "x" : 0,
-                "y" : 0,
-                "children": [
-                    {
-                        "class": "Classname2",
-                        "width": 10,
-                        "height": 40,
-                        "children_width": 0,
-                        "x" : 0,
-                        "y" : 0,
-                        "children": []
-                    },
-                    {
-                        "class": "Classname2",
-                        "width": 40,
-                        "height": 90,
-                        "children_width": 0,
-                        "x" : 0,
-                        "y" : 0,
-                        "children": []
-                    }
-                ]
-
-            }
-        ];
 
 
-        // při traversování dolů předávat potomkům výšku (parent.y + parent.height + LINE_LENGTH)
-        // strom procházet do šířky
-        // dojít až dolů a spočítat šířku listů
-        // iniciálně mají všechny nodes children_width = 0
-        // každý potomek udělá parent.children_width += node.width (+spacing)
+        const WHITE = 'white';
+        const GRAY = 'gray';
+        const BLACK = 'black';
 
 
+        var imported = document.createElement('script');
+        document.head.appendChild(imported);
 
-        function max(var1, var2) {
-            return var1 > var2 ? var1 : var2;
-        }
+        imported.src = 'https://d3js.org/d3.v4.min.js';
+        imported.onload = function () {
 
+            var data = [
+                {
+                    "class": "Classname",
+                    "metric1": 50,
+                    "metric2": 70,
+                    "children": [
+                        {
+                            "class": "Classname2",
+                            "metric1": 25,
+                            "metric2": 90,
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "class": "Classname2",
+                    "metric1": 50,
+                    "metric2": 90,
+                    "children": []
+                },
+                {
+                    "class": "Classname2",
+                    "metric1": 50,
+                    "metric2": 30,
+                    "children": []
+                },
+                {
+                    "class": "Classname2",
+                    "metric1": 50,
+                    "metric2": 70,
+                    "children": [
+                        {
+                            "class": "Classname2",
+                            "metric1": 10,
+                            "metric2": 40
+                        },
+                        {
+                            "class": "Classname2",
+                            "metric1": 40,
+                            "metric2": 90
+                        }
+                    ]
 
-        init_y = 1;
-        space = 5;
+                }
+            ];
 
-        current_x = 1;
-        current_y = init_y;
-        var classes = [];
+            var data2 = [
+                {
+                    "class": "Classname",
+                    "width": 50,
+                    "height": 70,
+                    "children_width": 0,
+                    "x" : 0,
+                    "y" : 0,
+                    "color" : WHITE,
+                    "children": [
+                        {
+                            "class": "Classname2",
+                            "width": 25,
+                            "height": 90,
+                            "children_width": 0,
+                            "x" : 0,
+                            "y" : 0,
+                            "color" : WHITE,
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "class": "Classname3",
+                    "width": 50,
+                    "height": 90,
+                    "children_width": 0,
+                    "x" : 0,
+                    "y" : 0,
+                    "color" : WHITE,
+                    "children": []
+                },
+                {
+                    "class": "Classname4",
+                    "width": 50,
+                    "height": 70,
+                    "children_width": 0,
+                    "x" : 0,
+                    "y" : 0,
+                    "color" : WHITE,
+                    "children": [
+                        {
+                            "class": "Classname5",
+                            "width": 10,
+                            "height": 40,
+                            "children_width": 0,
+                            "x" : 0,
+                            "y" : 0,
+                            "color" : WHITE,
+                            "children": []
+                        },
+                        {
+                            "class": "Classname6",
+                            "width": 60,
+                            "height": 90,
+                            "children_width": 0,
+                            "x" : 0,
+                            "y" : 0,
+                            "color" : WHITE,
+                            "children": []
+                        }
+                    ]
 
-        data2.forEach( function (node) {
-            // this is root node
-            node.y = current_y;
-            node.children.forEach( function (child) {
-                // ToDo: go deeper in tree
-                child.y = node.y + node.height;
-                child.x = current_x + node.children_width;
-                node.children_width += child.width + space;
-                classes.push(child);
+                },
+                {
+                    "class": "Classname7",
+                    "width": 50,
+                    "height": 30,
+                    "children_width": 0,
+                    "x" : 0,
+                    "y" : 0,
+                    "color" : WHITE,
+                    "children": []
+                }
+            ];
 
-            });
-            node.x = current_x;
+            init_y = 1;
+            space = 10;
+            LINE_LENGTH = 15;
+
+            current_x = 1;
             current_y = init_y;
-            current_x += max(node.children_width, node.width) + space;
-            classes.push(node);
-
-        });
 
 
+            data = data2;
 
+            var classes = [];
+            var lines = [];
 
-        // získat maximální šířku podstromu max_tree_width
-        // rodič bude mít x = max_tree_width / 2 - width / 2
-        // a y = 0
-        //
+            data.forEach( function (root) {
+                var stack = [];
 
+                root.parent = null;
+                stack.push(root);
 
+                while (stack.length > 0) {
+                    current = stack.pop();
 
-        // projít strom s už spočítanými pozicemi
-        // vygenerovat pole lines
+                    if (current.color == WHITE && current.children.length > 0) {
+                        stack.push(current);
+                        current.color = GRAY;
+                        current.y = current_y;
 
-  /**      var lines = [];
+                        // add all of his children to stack
+                        current.children.forEach( function (node) {
+                            node.parent = current;
 
-        items.forEach( function (item) {
-            console.log(item);
-            item.children.forEach( function (child) {
-                lines.push(
-                    {
-                        "x1": item.x + (item.metric1 / 2),
-                        "y1": item.y + item.metric2,
-                        "x2": child.x + (child.metric1 / 2),
-                        "y2": child.y + child.metric2
+                            stack.push(node)
+                        })
                     }
-                )
+                    else if (current.color == WHITE && current.children.length == 0) {
+                        // it is leaf
 
-            })
-        });
-   **/
+                        if (current.parent == null) {
+                            // it is also root
+                            current.x = current_x;
+                            current.y = current_y;
+                            current_y = init_y;
+                            current_x += current.width + space;
+                        } else {
+                            parrent = current.parent;
 
-        // function getLineBetweenClasses(class1, class2) {
-        // x1 = class1.x + (class1.width / 2)
-        // y1 = class1.y + class1.height
-        // x2 = class2.x + (class2.width / 2)
-        // y2 = class2.y + class2.height
+                            current.y = parrent.y + parrent.height + LINE_LENGTH;
+                            current.x = current_x + parrent.children_width;
+                            parrent.children_width += current.width + space;
+                        }
 
-        /**var lines = [
-            {
-                "x1": 25,
-                "y1": 70,
-                "x2": 25,
-                "y2": 80
-            },
-            {
-                "x1": 205,
-                "y1": 70,
-                "x2": 185,
-                "y2": 85
-            },
-            {
-                "x1": 205,
-                "y1": 70,
-                "x2": 213.5,
-                "y2": 85
+
+                        classes.push(current);
+                        current.color = BLACK;
+
+                    }
+                    else if (current.color == GRAY) {
+                        current.x = current_x + Math.max(current.children_width, current.width) / 2 - current.width / 2;
+                        current_y = init_y;
+                        current_x += Math.max(current.children_width, current.width) + space;
+                        classes.push(current);
+                        current.color = BLACK;
+                    }
+                }
+            });
+
+
+            console.log(classes);
+
+
+            function getLineBetweenClasses(parent, child) {
+                return {
+                    "x1": parent.x + (parent.width / 2),
+                    "y1": parent.y + parent.height,
+                    "x2": child.x + (child.width / 2),
+                    "y2": child.y
+                };
             }
-        ];
-         */
 
-        var width = 600;
-        var height = 700;
+            classes.forEach( function (item) {
+                if (!!item.parent) {
+                    lines.push(getLineBetweenClasses(item.parent, item))
+                }
+            });
 
-        var frame = d3.select(options.el).append("div")
-            .attr("style", "margin: 2em 2em");
+            var width = 600;
+            var height = 700;
 
-        frame.append("h2")
-            .text("This is my page with polymetric views!");
+            var frame = d3.select(options.el).append("div")
+                .attr("style", "margin: 2em 2em");
 
-        var div = frame.append("div")
-            .attr("style", "margin-top: 2em ");
+            frame.append("h2")
+                .text("This is my page with polymetric views!");
 
-        var svg = div.append("svg")
-            .attr("width", width)
-            .attr("height", height);
+            var div = frame.append("div")
+                .attr("style", "margin-top: 2em ");
 
-        console.log(classes);
+            var svg = div.append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-        var rect = svg.selectAll("rect")
-            .data(classes)
-            .enter().append("rect")
-            .attr("style", "fill:rgb(188, 188, 186);stroke-width:1;stroke:rgb(0,0,0)")
-            .attr("width", function(d) { return d.width; })
-            .attr("height", function(d) { return d.height; })
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
+            var rect = svg.selectAll("rect")
+                .data(classes)
+                .enter().append("rect")
+                .attr("style", "fill:rgb(188, 188, 186);stroke-width:1;stroke:rgb(0,0,0)")
+                .attr("width", function(d) { return d.width; })
+                .attr("height", function(d) { return d.height; })
+                .attr("x", function(d) { return d.x; })
+                .attr("y", function(d) { return d.y; });
 
-     /**   var line = svg.selectAll("line")
-            .data(lines)
-            .enter().append("line")
-            .attr("style", "stroke:rgb(30, 30, 29);stroke-width:2")
-            .attr("x1", function(d) { return d.x1; })
-            .attr("x2", function(d) { return d.x2; })
-            .attr("y1", function(d) { return d.y1; })
-            .attr("y2", function(d) { return d.y2; });
-*/
+            var line = svg.selectAll("line")
+                .data(lines)
+                .enter().append("line")
+                .attr("style", "stroke:rgb(30, 30, 29);stroke-width:2")
+                .attr("x1", function(d) { return d.x1; })
+                .attr("x2", function(d) { return d.x2; })
+                .attr("y1", function(d) { return d.y1; })
+                .attr("y2", function(d) { return d.y2; });
+
+        };
 
     };
 

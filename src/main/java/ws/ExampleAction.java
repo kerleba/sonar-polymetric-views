44b44e1d72f728b44c5aa0d650ea8f4beef913b0
Klara Erlebachova <klara.erlebachova@gmail.com>
2017-04-25 18:12:29 +0200

    Example action: read metric from metric framework

--- src/main/java/ws/ExampleAction.java
+++ src/main/java/ws/ExampleAction.java
@@ -1,6 +1,7 @@
 
 package main.java.ws;
 
+import main.java.framework.api.Database;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.RequestHandler;
 import org.sonar.api.server.ws.Response;
@@ -10,6 +11,8 @@ public class ExampleAction implements RequestHandler {
 
   @Override
   public void handle(Request request, Response response) throws Exception {
+    Database.getMeasures("loc");
+
     try (JsonWriter json = response.newJsonWriter()) {
       json.beginObject();
       json.prop("what", "wohoo");

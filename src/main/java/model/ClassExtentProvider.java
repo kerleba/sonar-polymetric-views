245163cb4b45fc3576d911473952b6ca18bb7f6c
Klara Erlebachova <klara.erlebachova@gmail.com>
2017-04-30 00:56:06 +0200

    ExtentProvider: verify that requeried measurement is avaliable

--- src/main/java/model/ClassExtentProvider.java
+++ src/main/java/model/ClassExtentProvider.java
@@ -2,6 +2,8 @@ package main.java.model;
 
 import main.java.framework.api.components.ClassComponent;
 import org.abego.treelayout.NodeExtentProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * ToDo: create javadoc
@@ -13,6 +15,9 @@ public class ClassExtentProvider implements NodeExtentProvider<ClassComponent> {
     private String widthMetric;
     private String heightMetric;
 
+    /** The logger object */
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     public ClassExtentProvider(String widthMetric, String heightMetric) {
         this.widthMetric = widthMetric;
         this.heightMetric = heightMetric;
@@ -20,11 +25,17 @@ public class ClassExtentProvider implements NodeExtentProvider<ClassComponent> {
 
     @Override
     public double getWidth(ClassComponent classComponent) {
+        if (!classComponent.getMeasures().containsKey(this.widthMetric)) {
+            this.log.warn("ClassComponent " + classComponent.getFileKey() + " does not have measurement for metric " + this.widthMetric);
+        }
         return classComponent.getMeasures().get(this.widthMetric);
     }
 
     @Override
     public double getHeight(ClassComponent classComponent) {
+        if (!classComponent.getMeasures().containsKey(this.heightMetric)) {
+            this.log.warn("ClassComponent " + classComponent.getFileKey() + " does not have measurement for metric " + this.heightMetric);
+        }
         return classComponent.getMeasures().get(this.heightMetric);
     }
 }
